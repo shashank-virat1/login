5de6e74197019657eabb1d203b3e633ff0e8419e
shashank-virat1 <157939122+shashank-virat1@users.noreply.github.com>
2024-01-28 09:48:45 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const connect = mongoose.connect("mongodb://localhost:27017/login");
+
+connect
+  .then(() => {
+    console.log("Database connected Successfully");
+  })
+
+  .catch(() => {
+    console.log("Database cannot be connected");
+  });
+
+const LoginSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const collection = new mongoose.model("user", LoginSchema);
+
+module.exports = collection;
